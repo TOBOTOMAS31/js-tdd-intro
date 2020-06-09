23a9f92346a1e5d4ad5ebc1f2cbd576d389e232b
TOBOTOMAS31 <thomas.couderc5@gmail.com>
2020-06-09 15:10:02 +0200

    challenge ok

--- test/Rectangle.test.js
+++ test/Rectangle.test.js
@@ -6,10 +6,13 @@ describe("Rectangle", () => {
     // check si les côtés sont égaux = true ou inégaux = false
     it("on check si les côtés sont égaux ou pas", () => {
       assert.ok(new Rectangle(1, 1), true, "true");
+    });
+    it("on check si les côtés sont pas égaux", () => {
       assert.ok(new Rectangle(1, 2), false, "false");
     });
   });
   describe("getArea", () => {
+    // check que surface rectangle est renvoyée 
     it("vérifie que la surface du rectangle est renvoyée", () => {
       const rectangle = new Rectangle(2, 5);
       const area = rectangle.getArea();
@@ -17,6 +20,7 @@ describe("Rectangle", () => {
     });
   });
   describe("getPerimeter", () => {
+    // check que superficie est renvoyée 
     it("check que la superficie est envoyée", () => {
       const rectangle = new Rectangle(3, 4);
       const perimeter = rectangle.getPerimeter();
